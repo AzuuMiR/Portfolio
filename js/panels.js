131eb71348c6f930990e36bf53d967b0490f8e16
Quentin <AzuuRa.R@hotmail.fr>
2020-02-25 09:24:23 +0100

    Initial commit

---
+++ js/panels.js
@@ -0,0 +1,259 @@
+// GAMES
+
+// SCARLET RADIANCE
+function openScarlet() {
+	document.getElementById("scarlet").style.visibility = "visible";
+	document.getElementById("scarlet").style.opacity = "1";
+    document.getElementById("scarlet").style.transitionDelay = "0s";
+}
+
+function closeScarlet() {
+	document.getElementById("scarlet").style.visibility = "hidden";
+	document.getElementById("scarlet").style.opacity = "0";
+    document.getElementById("scarlet").style.transition = "visibility 0s ease-in-out 0.5s, opacity 0.5s ease-in-out";
+}
+
+
+// DAHLIA NOIR
+function openDahlia() {
+    document.getElementById("dahlia").style.visibility = "visible";
+	document.getElementById("dahlia").style.opacity = "1";
+    document.getElementById("dahlia").style.transitionDelay = "0s";
+}
+
+function closeDahlia() {
+    document.getElementById("dahlia").style.visibility = "hidden";
+	document.getElementById("dahlia").style.opacity = "0";
+    document.getElementById("dahlia").style.transition = "visibility 0s ease-in-out 0.5s, opacity 0.5s ease-in-out";
+}
+
+
+// SPACESCAPE
+function openSpace() {
+    document.getElementById("space").style.visibility = "visible";
+	document.getElementById("space").style.opacity = "1";
+    document.getElementById("space").style.transitionDelay = "0s";
+}
+
+function closeSpace() {
+    document.getElementById("space").style.visibility = "hidden";
+	document.getElementById("space").style.opacity = "0";
+    document.getElementById("space").style.transition = "visibility 0s ease-in-out 0.5s, opacity 0.5s ease-in-out";
+}
+
+
+// LA FERME!
+function openLaferme() {
+    document.getElementById("laferme").style.visibility = "visible";
+	document.getElementById("laferme").style.opacity = "1";
+    document.getElementById("laferme").style.transitionDelay = "0s";
+}
+
+function closeLaferme() {
+    document.getElementById("laferme").style.visibility = "hidden";
+	document.getElementById("laferme").style.opacity = "0";
+    document.getElementById("laferme").style.transition = "visibility 0s ease-in-out 0.5s, opacity 0.5s ease-in-out";
+}
+
+
+// CIRCLES CYCLES
+function openCircles() {
+    document.getElementById("circles").style.visibility = "visible";
+	document.getElementById("circles").style.opacity = "1";
+    document.getElementById("circles").style.transitionDelay = "0s";
+}
+
+function closeCircles() {
+    document.getElementById("circles").style.visibility = "hidden";
+	document.getElementById("circles").style.opacity = "0";
+    document.getElementById("circles").style.transition = "visibility 0s ease-in-out 0.5s, opacity 0.5s ease-in-out";
+}
+
+
+// CONCEPTS
+
+// GW2 UI
+function openGW2UI() {
+	document.getElementById("gw2ui").style.visibility = "visible";
+	document.getElementById("gw2ui").style.opacity = "1";
+    document.getElementById("gw2ui").style.transitionDelay = "0s";
+}
+
+function closeGW2UI() {
+	document.getElementById("gw2ui").style.visibility = "hidden";
+	document.getElementById("gw2ui").style.opacity = "0";
+    document.getElementById("gw2ui").style.transition = "visibility 0s ease-in-out 0.5s, opacity 0.5s ease-in-out";
+}
+
+
+// GW2 PVP
+function openMelandru() {
+    document.getElementById("melandru").style.visibility = "visible";
+	document.getElementById("melandru").style.opacity = "1";
+    document.getElementById("melandru").style.transitionDelay = "0s";
+}
+
+function closeMelandru() {
+    document.getElementById("melandru").style.visibility = "hidden";
+	document.getElementById("melandru").style.opacity = "0";
+    document.getElementById("melandru").style.transition = "visibility 0s ease-in-out 0.5s, opacity 0.5s ease-in-out";
+}
+
+
+// ACR MISSION
+function openRevelations() {
+    document.getElementById("revelations").style.visibility = "visible";
+	document.getElementById("revelations").style.opacity = "1";
+    document.getElementById("revelations").style.transitionDelay = "0s";
+}
+
+function closeRevelations() {
+    document.getElementById("revelations").style.visibility = "hidden";
+	document.getElementById("revelations").style.opacity = "0";
+    document.getElementById("revelations").style.transition = "visibility 0s ease-in-out 0.5s, opacity 0.5s ease-in-out";
+}
+
+
+// GRAPH
+
+// SPOON TYPE ONE
+function openSpoon() {
+	document.getElementById("spoon").style.visibility = "visible";
+	document.getElementById("spoon").style.opacity = "1";
+    document.getElementById("spoon").style.transitionDelay = "0s";
+}
+
+function closeSpoon() {
+	document.getElementById("spoon").style.visibility = "hidden";
+	document.getElementById("spoon").style.opacity = "0";
+    document.getElementById("spoon").style.transition = "visibility 0s ease-in-out 0.5s, opacity 0.5s ease-in-out";
+}
+
+
+// S2000
+function openS2000() {
+    document.getElementById("s2000").style.visibility = "visible";
+	document.getElementById("s2000").style.opacity = "1";
+    document.getElementById("s2000").style.transitionDelay = "0s";
+}
+
+function closeS2000() {
+    document.getElementById("s2000").style.visibility = "hidden";
+	document.getElementById("s2000").style.opacity = "0";
+    document.getElementById("s2000").style.transition = "visibility 0s ease-in-out 0.5s, opacity 0.5s ease-in-out";
+}
+
+
+// COUNTACH
+function openCountach() {
+    document.getElementById("countach").style.visibility = "visible";
+	document.getElementById("countach").style.opacity = "1";
+    document.getElementById("countach").style.transitionDelay = "0s";
+}
+
+function closeCountach() {
+    document.getElementById("countach").style.visibility = "hidden";
+	document.getElementById("countach").style.opacity = "0";
+    document.getElementById("countach").style.transition = "visibility 0s ease-in-out 0.5s, opacity 0.5s ease-in-out";
+}
+
+
+// EDWARD KENWAY OUTFIT
+function openEdward() {
+    document.getElementById("edward").style.visibility = "visible";
+	document.getElementById("edward").style.opacity = "1";
+    document.getElementById("edward").style.transitionDelay = "0s";
+}
+
+function closeEdward() {
+    document.getElementById("edward").style.visibility = "hidden";
+	document.getElementById("edward").style.opacity = "0";
+    document.getElementById("edward").style.transition = "visibility 0s ease-in-out 0.5s, opacity 0.5s ease-in-out";
+}
+
+
+// ANKAMA ENVIRONMENT
+function openAnkama() {
+    document.getElementById("ankama").style.visibility = "visible";
+	document.getElementById("ankama").style.opacity = "1";
+    document.getElementById("ankama").style.transitionDelay = "0s";
+}
+
+function closeAnkama() {
+    document.getElementById("ankama").style.visibility = "hidden";
+	document.getElementById("ankama").style.opacity = "0";
+    document.getElementById("ankama").style.transition = "visibility 0s ease-in-out 0.5s, opacity 0.5s ease-in-out";
+}
+
+
+// INITIAL D FLAT DESIGN
+function openInitialD() {
+    document.getElementById("initiald").style.visibility = "visible";
+	document.getElementById("initiald").style.opacity = "1";
+    document.getElementById("initiald").style.transitionDelay = "0s";
+}
+
+function closeInitialD() {
+    document.getElementById("initiald").style.visibility = "hidden";
+	document.getElementById("initiald").style.opacity = "0";
+    document.getElementById("initiald").style.transition = "visibility 0s ease-in-out 0.5s, opacity 0.5s ease-in-out";
+}
+
+
+
+
+// OTHER EXP
+
+// TOUR
+function openTour() {
+	document.getElementById("tour").style.visibility = "visible";
+	document.getElementById("tour").style.opacity = "1";
+    document.getElementById("tour").style.transitionDelay = "0s";
+}
+
+function closeTour() {
+	document.getElementById("tour").style.visibility = "hidden";
+	document.getElementById("tour").style.opacity = "0";
+    document.getElementById("tour").style.transition = "visibility 0s ease-in-out 0.5s, opacity 0.5s ease-in-out";
+}
+
+
+// RICHARD
+function openRichard() {
+    document.getElementById("richard").style.visibility = "visible";
+	document.getElementById("richard").style.opacity = "1";
+    document.getElementById("richard").style.transitionDelay = "0s";
+}
+
+function closeRichard() {
+    document.getElementById("richard").style.visibility = "hidden";
+	document.getElementById("richard").style.opacity = "0";
+    document.getElementById("richard").style.transition = "visibility 0s ease-in-out 0.5s, opacity 0.5s ease-in-out";
+}
+
+
+// VCR
+function openVCR() {
+    document.getElementById("vcr").style.visibility = "visible";
+	document.getElementById("vcr").style.opacity = "1";
+    document.getElementById("vcr").style.transitionDelay = "0s";
+}
+
+function closeVCR() {
+    document.getElementById("vcr").style.visibility = "hidden";
+	document.getElementById("vcr").style.opacity = "0";
+    document.getElementById("vcr").style.transition = "visibility 0s ease-in-out 0.5s, opacity 0.5s ease-in-out";
+}
+
+// REPLAYER
+function openReplayer() {
+    document.getElementById("replayer").style.visibility = "visible";
+	document.getElementById("replayer").style.opacity = "1";
+    document.getElementById("replayer").style.transitionDelay = "0s";
+}
+
+function closeReplayer() {
+    document.getElementById("replayer").style.visibility = "hidden";
+	document.getElementById("replayer").style.opacity = "0";
+    document.getElementById("replayer").style.transition = "visibility 0s ease-in-out 0.5s, opacity 0.5s ease-in-out";
+}
